2bd3fb5105031f43f480cb24b42be23a00369839
mailto2020wa86196@wilp.bits-pilani.ac.in <kuruvasaikiran11@gmail.com>
2024-04-30 19:44:47 +0530

    added new function

---
+++ wims_demo/git_Demo/file_1.c
@@ -0,0 +1,9 @@
+int main(){
+     int n1, n2, sum;
+     printf("Enter 2 numbers");
+     scanf("%d %d", &n1, &n2);
+     //Adding 2 numbers
+     sum = n1 + n2;
+     printf("%d + %d = %d", n1, n2, sum);
+     return 0;
+}
